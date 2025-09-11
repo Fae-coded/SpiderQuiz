85a53b901a7b7576def86ba11523f67413923bd0
Sophie Beech <219414866+Fae-coded@users.noreply.github.com>
2025-09-11 15:13:22 +0100

    Add results functionality with styling

---
+++ assets/js/results.js
@@ -0,0 +1,6 @@
+//Get final score and display it on results page
+let finalScore = document.getElementById("finalScore");
+//Parse score from URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+const scoreParam = urlParams.get('score');
+finalScore.innerHTML = scoreParam + " / 8";
